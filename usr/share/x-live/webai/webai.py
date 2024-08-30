f9b1d99c8187f9faff0f506c9c383f081cfcdcc9
VerLinuxT <53666253+VerEnderT@users.noreply.github.com>
2024-08-30 10:22:52 +0200

    Add files via upload

--- usr/share/x-live/webai/webai.py
+++ usr/share/x-live/webai/webai.py
@@ -25,15 +25,25 @@ class MainWindow(QMainWindow):
         self.max = False
         self.browser = QWebEngineView()
         self.setCentralWidget(self.browser)
+        self.prozent = 0.43
 
         # Lade eine Webseite
         self.browser.setUrl(QUrl(URL))
         
+        # Auf das Signal screenGeometryChanged reagieren
+        self.screen = QApplication.primaryScreen()
+        self.screen.geometryChanged.connect(self.adjust_to_screen)
+
+        # Zusätzlich screenChanged-Signal abonnieren, falls sich der Bildschirm wechselt oder die Auflösung ändert
+        app.screenAdded.connect(self.screen_changed)
+        app.screenRemoved.connect(self.screen_changed)
+        
         # Bildschirmgröße berechnen
-        screen = app.primaryScreen().availableGeometry()
-        breite = int(screen.width()*0.43)
-        hoehe = int(screen.height())
-        xpos = int(screen.width() - breite)
+        screen_geometry = self.screen.availableGeometry()
+        breite = int(screen_geometry.width()*self.prozent)
+        hoehe = int(screen_geometry.height())
+        xpos = int(screen_geometry.width() - breite)
+        print(hoehe)
         
         # Fenster-Eigenschaften setzen
         self.setWindowIcon(QIcon('/usr/share/pixmaps/webai.png'))
@@ -61,14 +71,18 @@ class MainWindow(QMainWindow):
         copilot_action = QAction("MicorSoft Copilot", self)
         gemini_action = QAction("Google Gemini", self)
         chatgpt_action = QAction("OpenAI Chat-GPT", self)
+        fragai_action = QAction("Frag die Ki", self)
 
         self.change_menu.addAction(copilot_action)
         self.change_menu.addAction(gemini_action)
         self.change_menu.addAction(chatgpt_action)
+        self.change_menu.addAction(fragai_action)
 
         copilot_action.triggered.connect(lambda: self.change_url('https://copilot.microsoft.com/'))
         gemini_action.triggered.connect(lambda: self.change_url('https://gemini.google.com/'))
         chatgpt_action.triggered.connect(lambda: self.change_url('https://chat.openai.com/'))
+        fragai_action.triggered.connect(lambda: self.change_url('https://www.aichatting.net/de/ask-ai/'))
+        
 
         open_action.triggered.connect(self.toggle_window)
         exit_action.triggered.connect(QApplication.instance().quit)
@@ -87,6 +101,35 @@ class MainWindow(QMainWindow):
 
         # Start minimized to tray
         self.hide()
+        
+
+    def adjust_to_screen(self):
+        """Passt die Fenstergröße an die aktuelle Bildschirmgeometrie an."""
+        screen_geometry = self.screen.availableGeometry()
+        #self.setGeometry(screen_geometry)
+        print("Bildschirmgröße angepasst:", screen_geometry)
+        print(screen_geometry.height())
+        breite = int(screen_geometry.width()*self.prozent)
+        max_breite = int(screen_geometry.width())
+        hoehe = int(screen_geometry.height())
+        xpos = int(screen_geometry.width() - breite)
+        #print(hoehe)
+        
+        if self.max == True:
+            self.setGeometry(0, 0, max_breite, hoehe)
+            #print("show max")
+            #print(breite)
+        else:
+            self.setGeometry(xpos, 0, breite, hoehe)
+            #print("show no max")
+            #print(breite)
+                
+    def screen_changed(self):
+        """Ruft die aktuelle Bildschirmgeometrie nach einer Änderung ab."""
+        self.screen = QApplication.primaryScreen()
+        print("Bildschirm geändert. Neue Geometrie:", self.screen.availableGeometry())
+        self.adjust_to_screen()
+
 
     def change_url(self, url):
         self.browser.setUrl(QUrl(url))
@@ -119,22 +162,42 @@ class MainWindow(QMainWindow):
             self.hide()
         else:
             self.show()
+            screen_geometry = self.screen.availableGeometry()
+            breite = int(screen_geometry.width()*self.prozent)
+            max_breite = int(screen_geometry.width())
+            hoehe = int(screen_geometry.height())
+            xpos = int(screen_geometry.width() - breite)
+            #print(hoehe)
+        
+            if self.max == True:
+                self.setGeometry(0, 0, max_breite, hoehe)
+                #print("show max")
+                #print(breite)
+            else:
+                self.setGeometry(xpos, 0, breite, hoehe)
+                #print("show no max")
+                #print(breite)
+                
             self.raise_()  # Bringt das Fenster in den Vordergrund
 
     def max_window(self):
-        screen = app.primaryScreen().availableGeometry()
-        breite = int(screen.width()*0.33)
+        #screen = app.primaryScreen().availableGeometry()
+        screen = self.screen.availableGeometry()
+        breite = int(screen.width()*self.prozent)
         max_breite = int(screen.width())
         hoehe = int(screen.height())
         xpos = int(screen.width() - breite)
+        print("max")
         if self.max:
             self.setGeometry(xpos, 0, breite, hoehe)
             self.max = False
             self.max_action.setText("Maximieren")
+            print("no_max")
         else:
             self.setGeometry(0, 0, max_breite, hoehe)
             self.max = True
             self.max_action.setText("Normale Größe")
+            print("max")
 
     def closeEvent(self, event):
         event.ignore()
